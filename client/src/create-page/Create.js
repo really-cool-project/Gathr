39e94e5f13f01af448c149db844bb7ec47b0da96
Travis Hewitt <travis.j.hewitt@gmail.com>
2017-10-02 15:51:51 -0600

    did some more styling and made the navbar sticky and have content flow underneath it

--- client/src/create-page/Create.js
+++ client/src/create-page/Create.js
@@ -5,26 +5,30 @@ class Create extends Component {
         return (
             <div>
                 <div className="create-supper">
-                    <p className = "create-supertitle">Create Your Supper Club</p>
-                    <p className = "create-subtitle">Please consider the standards as you create your supperclub.</p>
-                    <p className = "create-titles">Supperclub Title</p>
-                    <textarea name="textarea" className = "create-text-area"
+                    <p className="create-supertitle">Create Your Supper Club</p>
+                    <p className="create-subtitle">Please consider the standards as you create your supperclub.</p>
+                    <p className="create-titles">Supperclub Title</p>
+                    <textarea name="textarea" className="create-text-area"
                         rows="10" cols="100" placeholder="Write your title here!"></textarea>
-                    <p className = "create-titles">Chef bio</p>
-                    <p className = "create-subtitle">Tell us your story. How did you get into cooking? What cuisine influences you the most? Where did you learn?</p>
-                    <textarea name="textarea" className = "create-text-area"
+                    <p className="create-titles">Chef bio</p>
+                    <p className="create-subtitle">Tell us your story. How did you get into cooking? What cuisine influences you the most? Where did you learn?</p>
+                    <textarea name="textarea" className="create-text-area"
                         rows="10" cols="100" placeholder="Tell us your story here!"></textarea>
-                    <p className = "create-titles">Supperclub description</p>
-                    <p className = "create-subtitle">Tell us what you plan to serve and how you plan to present it. You can put a menu here, or just a small description of your offerings.</p>
-                    <textarea name="textarea" className = "create-text-area"
+                    <p className="create-titles">Supperclub description</p>
+                    <p className="create-subtitle">Tell us what you plan to serve and how you plan to present it. You can put a menu here, or just a small description of your offerings.</p>
+                    <textarea name="textarea" className="create-text-area"
                         rows="10" cols="100" placeholder="Tell us about your culinary offerings here!"></textarea>
-                    <p className = "create-titles">Date and Time</p>
-                    <p className = "create-subtitle">Tell us the time and date you plan to hold your supperclub.</p>
-                    <p className = "create-titles">Photos</p>
-                    <p className = "create-subtitle">Give us some visual stimulation! You can supply photos of your own food, or some photos that influenced your offerings</p>
-                    <input type="text" placeholder="Image 1" />
-                    <input type="text" placeholder="Image 2" />
-                    <input type="text" placeholder="Image 3" />
+                    <p className="create-titles">Date and Time</p>
+                    <p className="create-subtitle">Tell us the time and date you plan to hold your supperclub.</p>
+                    <p className="create-titles">Photos</p>
+                    <p className="create-subtitle">Give us some visual stimulation! You can supply photos of your own food, or some photos that influenced your offerings</p>
+                    <div className="create-img-container">
+                        <input className="img-text-box" type="text" placeholder="Image 1" />
+                        <input className="img-text-box img-text-box-center" type="text" placeholder="Image 2" />
+                        <input className="img-text-box" type="text" placeholder="Image 3" />
+                    </div>
+                    <button className="submit-btn">Submit</button>
+
                 </div>
             </div>
         );
