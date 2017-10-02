7123e6a24ee041b0cc681e98becaa0b7eff3f9b4
Rachel Howell <rachel.mhowell96@gmail.com>
2017-10-02 12:27:00 -0600

    apply page

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import NavbarContainer from './navbar/NavbarContainer.js';
 import CheckboxContainer from './checkbox/CheckboxContainer.js';
+import ApplyContainer from "./apply/ApplyContainer.js";
 import './styles/Navbar.css';
 import './styles/CheckboxPage.css';
 import './styles/typography.css';
+import "./styles/apply.css";
 
 class App extends Component {
     render() {
