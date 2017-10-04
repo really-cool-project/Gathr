12a8b3b2eb6dd20230c1237caf7934e02b133083
Travis Hewitt <travis.j.hewitt@gmail.com>
2017-10-04 17:26:52 -0600

    almost finished with server stuff

--- client/src/index.js
+++ client/src/index.js
@@ -28,7 +28,7 @@ class App extends Component {
         return (
             <div>
                 <NavbarContainer />
-                <SignUpContainer />
+                
             </div>
         );
     }
