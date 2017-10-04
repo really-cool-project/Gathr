6c34a3f68ea4aacf54ad891c27f1e334de27f41c
Rachel Howell <rachel.mhowell96@gmail.com>
2017-10-04 11:26:25 -0600

    updated routing for apply

--- client/src/apply/Apply.js
+++ client/src/apply/Apply.js
@@ -9,7 +9,7 @@ class Apply extends Component {
                     <p className="applyDescription">As you pitch, remember that guests look for passion and good execution</p>
                 </div>
                 <div className="applyBody">
-                    <textarea className="applyTextarea" placeholder="Tell us how your supper will be unique" rows="12" cols="80"/>
+                    <textarea className="applyTextarea" placeholder="Tell us how your supper will be unique" rows="12" cols="95"/>
                     <button className="applySubmit">Pitch my supper!</button>
                 </div>
             </div>
