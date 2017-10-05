24748847cccf59f18f761268d4b0109ad775e7a6
Travis Hewitt <travis.j.hewitt@gmail.com>
2017-10-05 13:56:50 -0600

    -A is magic.

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
+
 const config = require("./config");
 const supperRoute = require("./routes/supper");
 const authRoute = require("./routes/auth");
 const profileRoute = require("./routes/profile");
+const publicRoute = require("./routes/public.js")
 
 const PORT = config.PORT;
 const app = express();
@@ -20,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/supper", supperRoute);
 app.use("/auth", authRoute);
 app.use("/profile", profileRoute);
+app.use("/public", publicRoute);
 
 mongoose.connect(config.db, () => {
     console.log("CONNECTED ON " + config.db);
