e1fbcb685d2e441391c9e3b665d94a93112bddae
Rachel Howell <rachel.mhowell96@gmail.com>
2017-10-06 09:28:37 -0600

    worked on landing css

--- client/src/landing/ItemComponent.js
+++ client/src/landing/ItemComponent.js
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 class ItemComponent extends Component {
     render() {
         return (
-            <div className="gallery-wrapper">
-                    <img className="supper-image" src={this.props.photo} alt=""/>
+            <div class="masonry">
+                <div class="item">
+                    <img className="supper-img" src={this.props.photo} alt=""/>
                     <div className="supper-wrapper">
                         <p className="supper-title"> {this.props.title} </p>
                         <p className="supper-date"> {this.props.date} </p>
@@ -13,13 +14,11 @@ class ItemComponent extends Component {
                         <p className="supper-spots"> {this.props.spots} </p>
                     </div>
             </div>
+                    
+
+     </div>
         );
     }
 }
-
-                        {/* <p className="supper-title"> {this.props.title} </p>
-                        <p className="supper-date"> {this.props.date} </p>
-                        <p className="supper-price"> {this.props.price} </p>
-                        <p className="supper-spots"> {this.props.spots} </p> */}
 
 export default ItemComponent;
